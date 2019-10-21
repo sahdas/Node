bd4e09a0f99b929270fc0033e78e62dcce784e83
sahdas <52988348+sahdas@users.noreply.github.com>
2019-10-21 22:02:50 +0530

    Update TimerHelloWorld.js

--- TimerHelloWorld.js
+++ TimerHelloWorld.js
@@ -1,8 +1,9 @@
 //This code will print 'Hello World' in every second for 5 seconds and then print 'Done' and close
+//Set Interval for every second and print 'Hello World'
 var interval = setInterval(function(){ 
   console.log('Hello World'); 
 }, 1000);
-
+//After 5 sec, set timeout and print 'Done'
 setTimeout(function() { 
   clearInterval(interval); 
   console.log('Done');
